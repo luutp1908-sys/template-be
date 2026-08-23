f10591997287c1575bed1aeee6244d83b087bc97
Luu <luu@Khoas-MacBook-Pro-2.local>
2026-08-24 03:25:40 +0700

    remove debug step and update AWS role ARN to use secrets

--- infra/environments/prod/versions.tf
+++ infra/environments/prod/versions.tf
@@ -1,11 +1,20 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket       = "template-saas-prod-terraform-state-764800440966"
+    key          = "prod/terraform.tfstate"
+    region       = "ap-southeast-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
